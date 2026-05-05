3045c925dfc430d5f5128f59f1a67b978248b9ab
umutstudio <baharelidenk@gmail.com>
2026-05-05 17:39:16 +0300

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+app.post("/generate", (req, res) => {
+  const { os, programs } = req.body;
+  let output = "";
+
+  programs.forEach(p => {
+    let ext = os === "windows" ? "bat" : "sh";
+    const scriptPath = path.join(__dirname, "../installers", os, `${p.toLowerCase()}.${ext}`);
+    if (fs.existsSync(scriptPath)) {
+      output += fs.readFileSync(scriptPath, "utf8") + "\n";
+    }
+  });
+
+  res.send(output);
+});
+
+app.listen(3000, () => console.log("Server çalışıyor: http://localhost:3000"));
